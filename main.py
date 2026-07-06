e1fa1e9f4688d3b0c0dbc5b84dcb6cb495b5f543
Nupur Mamgain <Nupurmamgain2506@gmail.com>
2026-07-06 07:44:06 +0530

    force redeploy with railway backend

--- main.py
+++ main.py
@@ -14,7 +14,8 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173","https://queryit-frontend.vercel.app/","https://queryit-git-main-nupurmamgains-projects.vercel.app/",
+                   "https://queryit-84jrt8mq7-nupurmamgains-projects.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
